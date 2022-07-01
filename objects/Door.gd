517d664026997245a0f02c913f5e341f84241fc0
Willy <willybc06@gmail.com>
2022-07-01 15:15:28 -0300

    update assets character and sister

--- objects/Door.gd
+++ objects/Door.gd
@@ -8,8 +8,6 @@ func _ready():
 	next_position = get_node("DoorDestination").get_global_position()
 
 
-func _on_Door_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+func _on_Door_body_entered(body):
 	if body.name == "Player":
 		body.set_position(next_position)
-
-
